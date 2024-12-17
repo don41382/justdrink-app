7722ea8d5742cab142a15d72340101860dd7ab4b
Felix Eckhardt <felix.e@gmx.de>
2024-12-17 21:33:15 +0100

    fix spelling

--- src/routes/updater/+page.svelte
+++ src/routes/updater/+page.svelte
@@ -104,11 +104,11 @@
             <img src="{data.iconPath}" alt="App Logo" class="w-14 h-14 mr-4">
             <div>
                 <h1 class="text-xl font-semibold text-gray-800">Up to date.</h1>
-                <p class="text-gray-500">Their is no newer version.</p>
+                <p class="text-gray-500">There is no newer version.</p>
             </div>
         </div>
 
-        <p class="text-gray-600 mb-6">You are currently on the newest version. Please check later.</p>
+        <p class="text-gray-600 mb-6">You are currently on the latest version. Please check later.</p>
 
         <div class="flex justify-end space-x-3">
             <button class="bg-gray-300 text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 cursor-pointer"
