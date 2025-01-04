60cf9e424e7e72df7e0815149aa137a73a88893f
Felix Eckhardt <felix.e@gmx.de>
2025-01-04 16:09:33 +0100

    script: change to macos naming

--- src-tauri/videos/package-videos.sh
+++ src-tauri/videos/package-videos.sh
@@ -3,7 +3,7 @@
 # Check for platform argument
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <platform>"
-    echo "Platform options: mac, windows"
+    echo "Platform options: macos, windows"
     exit 1
 fi
 
@@ -13,16 +13,16 @@ ZIP_NAME="$PLATFORM.zip"
 rm -f "$ZIP_NAME"
 
 # Determine the file pattern and handle platform-specific logic
-if [ "$PLATFORM" == "mac" ]; then
+if [ "$PLATFORM" == "macos" ]; then
     FILE_PATTERN="*.mov"
-    echo "Zipping .mov files for mac..."
+    echo "Zipping .mov files for macos..."
     zip -r "$ZIP_NAME" $(find . -type f -name "$FILE_PATTERN")
 elif [ "$PLATFORM" == "windows" ]; then
     FILE_PATTERN="*.webm"
     echo "Zipping .webm files for windows..."
     zip -r "$ZIP_NAME" bg-h264.mov $(find . -type f -name "$FILE_PATTERN")
 else
-    echo "Invalid platform specified. Use 'mac' or 'windows'."
+    echo "Invalid platform specified. Use 'macos' or 'windows'."
     exit 1
 fi
 
